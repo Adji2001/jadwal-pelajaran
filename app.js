c9da8de54b582531d4d935f1c6cab458f24b8f43
Adji2001 <abdulm4j1d182@gmail.com>
2022-11-20 04:39:33 -0800

    membuat folder public

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 4000;
 const expressLayouts = require('express-ejs-layouts');
 
-app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(expressLayouts);
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 	res.render('index', {
